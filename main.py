54e65d95bb9ff7c2fc2c046479cf5b262dde4f1d
AgaevDanik <agaevdaniik@gmail.com>
2022-01-18 23:36:34 +0100

    class Frame with methods and their tests

---
+++ main.py
@@ -0,0 +1,44 @@
+class Frame:
+
+    def __init__(self, data_list: list, border_sign: str, name: str):
+        self.data_list = data_list
+        self.border_sign = border_sign
+        self.name = name
+
+    def get_a_border_width(self) -> int:
+        return self.get_max_len() + 4
+
+    def get_max_len(self) -> int:
+        max_len = 0
+        for i in self.data_list:
+            if i.__len__() > 0:
+                max_len = i.__len__()
+
+        return max_len
+
+    def get_delta(self, item) -> int:
+        return self.get_max_len() - item.__len__()
+
+    def print_frame(self) -> None:
+        self.print_horizontal_border()
+        self.print_content()
+        self.print_horizontal_border()
+
+    def print_content(self) -> None:
+        for item in self.data_list:
+            print(self.make_content_row(item))
+
+    def make_content_row(self, item):
+        return f"{self.border_sign} " + item + (" " * (self.get_delta(item) + 1)) + self.border_sign
+
+    def print_horizontal_border(self) -> None:
+        print(self.border_sign * self.get_a_border_width())
+
+
+if __name__ == '__main__':
+    pyramid = Frame(['    _',
+                     '   ___',
+                     '  _____',
+                     ' _______',
+                     '_________'], '*', 'pyramid')
+    pyramid.print_frame()
---
+++ tests.py
@@ -0,0 +1,23 @@
+import unittest
+from main import Frame
+
+
+class TestFrame(unittest.TestCase):
+    def setUp(self) -> None:
+        self.A = Frame(['test', 'testtest'], '*', 'test')
+
+    def test_get_max_len(self):
+        self.assertEqual(self.A.get_max_len(), 8)
+
+    def test_get_a_border_width(self):
+        self.assertEqual(self.A.get_a_border_width(), 12)
+
+    def test_get_delta(self):
+        self.assertEqual(self.A.get_delta('test'), 4)
+
+    def test_make_kontent_row(self):
+        self.assertEqual(self.A.make_content_row('test'), '* test     *')
+
+
+if __name__ == '__main__':
+    unittest.main()
